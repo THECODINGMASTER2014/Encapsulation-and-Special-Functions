1b8aa18deb8a8f685099770f73c89cdd0d4dc96a
THECODINGMASTER2014 <talwarmoksh@gmail.com>
2026-04-09 12:27:06 +0530

    a

---
+++ Index.py
@@ -0,0 +1,18 @@
+class I_Am_Inside_This_Haunted_House_Help_Me_Escape:
+    _privateVar = 27;
+    
+    def __privMeth(self):
+        print("I'm Inside This_Haunted_House_Help_Me_Escape")
+
+    def hello(self):
+        print("I_Am_Inside_This_Haunted_House_Help_Me_Escape Private Variable Value :",I_Am_Inside_This_Haunted_House_Help_Me_Escape._privateVar)
+
+print('I_Am_Inside_This_Haunted_House_Help_Me_Escape')
+foo = I_Am_Inside_This_Haunted_House_Help_Me_Escape()
+foo.hello()
+foo.__privMeth
+
+
+    
+
+
